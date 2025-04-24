7b10fbbf5e77d1a20b26874c2fa70656924d6b72
AlexOvch <qa.ovcharenko@gmail.com>
2025-04-24 10:41:33 +0300

    HomeWork_10_update_01

--- lesson_10/homework_10.py
+++ lesson_10/homework_10.py
@@ -1,35 +1,41 @@
 import unittest
 from log_event_function import log_event
 
+def read_last_log_line(log_path="login_system.log"):
+    with open(log_path, "r") as f:
+        lines = f.readlines()
+        last_line = lines[-1] if lines else ""
+        return lines, last_line
+
 
 class TestHomework10(unittest.TestCase):
 
-    def _run_test(self, username, status, expected_level, test_name):
-        log_event(username=username, status=status)
-
-        with open("login_system.log", "r") as f:
-            lines = f.readlines()
-            last_line = lines[-1] if lines else ""
-
-        # Просто убираем try/except, так как unittest сам поймает ошибки
-        self.assertTrue(lines, "Лог-файл пуст")
-        self.assertIn(expected_level, last_line, f"{test_name}: Ожидался уровень {expected_level}")
-        self.assertIn(username, last_line, f"{test_name}: Username не найден в логе")
-        self.assertIn(status, last_line, f"{test_name}: Status не найден в логе")
-
-    # Тести
-
     def test_01_success_status(self):
-        self._run_test(username="alex", status="success", expected_level="INFO", test_name="test_01_success_status")
+        username = "Ales"
+        status = "success"
+        log_event(username, status)
+        _, last_line = read_last_log_line()
+        self.assertIn("INFO", last_line)
+        self.assertIn(username, last_line)
+        self.assertIn(status, last_line)
 
     def test_02_expired_status(self):
-        self._run_test(username="Nick", status="expired", expected_level="WARNING", test_name="test_02_expired_status")
+        username = "Ales"
+        status = "expired"
+        log_event(username, status)
+        _, last_line = read_last_log_line()
+        self.assertIn("WARNING", last_line)
+        self.assertIn(username, last_line)
+        self.assertIn(status, last_line)
 
-    def test_03_invalid_status(self):
-        self._run_test(username="Dima", status="failed", expected_level="ERROR", test_name="test_03_invalid_status")
-
-    def test_04_empty_username(self):
-        self._run_test(username="", status="success", expected_level="INFO", test_name="test_empty_username")
+    def test_03_failed_status(self):
+        username = "Ales"
+        status = "failed"
+        log_event(username, status)
+        _, last_line = read_last_log_line()
+        self.assertIn("ERROR", last_line)
+        self.assertIn(username, last_line)
+        self.assertIn(status, last_line)
 
 if __name__ == "__main__":
     unittest.main()
